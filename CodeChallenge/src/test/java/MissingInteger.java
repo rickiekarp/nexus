00959364bf7bc93feb8e31300ad5b67ea3e05f48
Rickie Karp <rickie.karp@gmail.com>
2018-09-01 08:31:29 +0200

    Update MissingInteger Test

--- CodeChallenge/src/test/java/MissingInteger.java
+++ CodeChallenge/src/test/java/MissingInteger.java
@@ -30,8 +30,8 @@ public class MissingInteger {
     private int getResult(int[] array) {
         int result = 1;
 
-        for (int i = 1; i <= array.length; i++) {
-            if (array[i-1] > 0) {
+        for (int i = 0; i <= array.length; i++) {
+            if (array[i] > 0) {
                 if (contains(array, i)) {
                     result++;
                 } else {
