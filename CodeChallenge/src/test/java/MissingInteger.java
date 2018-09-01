9eb4d0c49c1c6b2d8cf8131d2ab2046a0f24b3bb
Rickie Karp <rickie.karp@gmail.com>
2018-09-01 08:28:21 +0200

    Add CodeChallenge project

---
+++ CodeChallenge/src/test/java/MissingInteger.java
@@ -0,0 +1,90 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.HashSet;
+
+/**
+ * Write a function:
+ *
+ * class Solution { public int solution(int[] A); }
+ *
+ * that, given an array A of N integers, returns the smallest positive integer (greater than 0) that does not occur in A.
+ *
+ * For example, given A = [1, 3, 6, 4, 1, 2], the function should return 5.
+ *
+ * Given A = [1, 2, 3], the function should return 4.
+ *
+ * Given A = [−1, −3], the function should return 1.
+ *
+ * Write an efficient algorithm for the following assumptions:
+ *
+ * N is an integer within the range [1..100,000];
+ * each element of array A is an integer within the range [−1,000,000..1,000,000].
+ */
+public class MissingInteger {
+
+    private int[] one = {1,3,6,4,1,2};
+    private int[] two = {1,2,3};
+    private int[] three = {-1,-3};
+
+    private int getResult(int[] array) {
+        int result = 1;
+
+        for (int i = 1; i <= array.length; i++) {
+            if (array[i-1] > 0) {
+                if (contains(array, i)) {
+                    result++;
+                } else {
+                    result = i;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private int getResultNew(int[] array) {
+        HashSet<Integer> seen = new HashSet<>();
+        int min = 1;
+
+        for (int anArray : array) {
+            if (anArray > 0) {
+                seen.add(anArray);
+            }
+        }
+
+        for(int i = 1 ; i < Integer.MAX_VALUE; i++) {
+            if(!seen.contains(i)) {
+                return i;
+            }
+        }
+
+        return min;
+    }
+
+    private boolean contains(final int[] array, final int key) {
+        for (final int i : array) {
+            if (i == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Test
+    public void testOne() {
+        Assert.assertEquals(5, getResultNew(one));
+    }
+
+    @Test
+    public void testTwo() {
+        Assert.assertEquals(4, getResultNew(two));
+    }
+
+    @Test
+    public void testThree() {
+        Assert.assertEquals(1, getResultNew(three));
+    }
+
+}
