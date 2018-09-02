659becc970ccdf937b5944ee1c0262363bfc76fb
Rickie Karp <rickie.karp@gmail.com>
2018-09-02 13:15:23 +0200

    Update MissingInteger Test

--- CodeChallenge/src/test/java/MissingInteger.java
+++ CodeChallenge/src/test/java/MissingInteger.java
@@ -27,11 +27,16 @@ public class MissingInteger {
     private int[] two = {1,2,3};
     private int[] three = {-1,-3};
 
+    /**
+     * O(n**2)
+     * @param array
+     * @return
+     */
     private int getResult(int[] array) {
         int result = 1;
 
-        for (int i = 0; i <= array.length; i++) {
-            if (array[i] > 0) {
+        for (int i = 1; i <= array.length; i++) {
+            if (array[i-1] > 0) {
                 if (contains(array, i)) {
                     result++;
                 } else {
@@ -44,6 +49,11 @@ public class MissingInteger {
         return result;
     }
 
+    /**
+     * O(n)
+     * @param array Array to search in
+     * @return Smallest positive integer
+     */
     private int getResultNew(int[] array) {
         HashSet<Integer> seen = new HashSet<>();
         int min = 1;
@@ -74,17 +84,33 @@ public class MissingInteger {
 
     @Test
     public void testOne() {
-        Assert.assertEquals(5, getResultNew(one));
+        Assert.assertEquals(5, getResult(one));
     }
 
     @Test
     public void testTwo() {
-        Assert.assertEquals(4, getResultNew(two));
+        Assert.assertEquals(4, getResult(two));
     }
 
     @Test
     public void testThree() {
+        Assert.assertEquals(1, getResult(three));
+    }
+
+    @Test
+    public void testOneNew() {
+        Assert.assertEquals(5, getResultNew(one));
+    }
+
+    @Test
+    public void testTwoNew() {
+        Assert.assertEquals(4, getResultNew(two));
+    }
+
+    @Test
+    public void testThreeNew() {
         Assert.assertEquals(1, getResultNew(three));
     }
+
 
 }
