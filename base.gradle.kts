f8aaf2f258c866c44b7aa4377328be364c3fb7ab
Rickie Karp <rickie.karp@gmail.com>
2018-10-21 15:47:54 +0200

    Add base.gradle.kts properties file

---
+++ base.gradle.kts
@@ -0,0 +1,12 @@
+extra.set("gradleWrapper", "4.9")
+extra.set("gradleWrapperVersion", "5.0-milestone-1")
+
+extra.set("kotlinVersion", "1.2.71")
+
+extra.set("dokkaVersion", "0.9.17")
+extra.set("detektVersion", "1.0.0.RC9.2")
+
+extra.set("spotbugsVersion", "1.6.4")
+
+extra.set("junit4Version", "4.12")
+extra.set("junit5Version", "5.3.1")
