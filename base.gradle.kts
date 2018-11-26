284dbf24da1d02bd064fd632a676f5bd9bec758b
Rickie Karp <rickie.karp@gmail.com>
2018-11-26 12:25:33 +0100

    Update kotlin version

--- base.gradle.kts
+++ base.gradle.kts
@@ -1,7 +1,7 @@
 extra.set("gradleWrapper", "4.10.2")
 extra.set("gradleWrapperVersion", "5.0-milestone-1")
 
-extra.set("kotlinVersion", "1.2.71")
+extra.set("kotlinVersion", "1.3.10")
 
 extra.set("dokkaVersion", "0.9.17")
 extra.set("detektVersion", "1.0.0-RC11")
