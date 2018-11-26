5b6992129a65e795a085d65cfb52fe1fce8ddd06
Rickie Karp <rickie.karp@gmail.com>
2018-11-26 11:59:57 +0100

    Update detekt version

--- base.gradle.kts
+++ base.gradle.kts
@@ -1,10 +1,10 @@
-extra.set("gradleWrapper", "4.9")
+extra.set("gradleWrapper", "4.10.2")
 extra.set("gradleWrapperVersion", "5.0-milestone-1")
 
 extra.set("kotlinVersion", "1.2.71")
 
 extra.set("dokkaVersion", "0.9.17")
-extra.set("detektVersion", "1.0.0.RC9.2")
+extra.set("detektVersion", "1.0.0-RC11")
 
 extra.set("spotbugsVersion", "1.6.4")
 
