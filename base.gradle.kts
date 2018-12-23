e7dd2e296597e8d047150fa7974e546d35407e2f
Rickie <contact@rickiekarp.net>
2018-12-23 13:30:35 +0100

    Update kotlin version

--- base.gradle.kts
+++ base.gradle.kts
@@ -1,6 +1,6 @@
 extra.set("gradleVersion", "5.0")
 
-extra.set("kotlinVersion", "1.3.10")
+extra.set("kotlinVersion", "1.3.11")
 
 extra.set("dokkaVersion", "0.9.17")
 extra.set("detektVersion", "1.0.0-RC11")
