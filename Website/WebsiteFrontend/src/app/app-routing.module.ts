3371c8727b9a247e12255286f30c1faafc09517e
Rickie Karp <rickie.karp@gmail.com>
2019-02-23 21:37:38 +0100

    Website: useHash=true in RouterModule

--- Website/WebsiteFrontend/src/app/app-routing.module.ts
+++ Website/WebsiteFrontend/src/app/app-routing.module.ts
@@ -13,7 +13,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
    RouterModule.forRoot(routes, {
-         scrollPositionRestoration: 'enabled'
+         scrollPositionRestoration: 'enabled',
+         useHash: true
        })
   ],
   exports: [ RouterModule ]
