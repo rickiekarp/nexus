3a3e58a4c660e8484981b4be95acf33ec4f3aef3
Rickie Karp <rickie.karp@gmail.com>
2019-02-24 18:18:21 +0100

    Website: Check in missing component

--- Website/WebsiteFrontend/src/app/app.module.ts
+++ Website/WebsiteFrontend/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { PersonalComponent } from './components/personal/personal.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ContactService } from './service/contact.service';
 import { SafePipe } from './core/pipe/safepipe.service';
-import { AboutComponent } from './about/about.component';
+import { AboutComponent } from './components/about/about.component';
 
 import { AppRoutingModule } from './app-routing.module';
 
