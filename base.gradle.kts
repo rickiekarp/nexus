431fd7b77576fc4a7c458dac703339d8505ee9c1
Rickie Karp <rickie.karp@gmail.com>
2019-03-09 15:51:12 +0100

    Update base.gradle.kts

--- base.gradle.kts
+++ base.gradle.kts
@@ -3,9 +3,12 @@ extra.set("gradleVersion", "5.0")
 extra.set("kotlinVersion", "1.3.21")
 
 extra.set("dokkaVersion", "0.9.17")
-extra.set("detektVersion", "1.0.0-RC11")
+extra.set("detektVersion", "1.0.0-RC14")
 
 extra.set("spotbugsVersion", "1.6.4")
 
-extra.set("junit4Version", "4.12")
-extra.set("junit5Version", "5.3.1")
+extra.set("junitVersion", "5.3.1")
+
+extra.set("springBootVersion", "2.1.0.RELEASE")
+
+extra.set("flywayVersion", "5.2.1")
