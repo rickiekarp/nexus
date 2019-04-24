c4a10293cf910e75c6146032a5cc2168977c5473
Rickie Karp <rickie.karp@gmail.com>
2019-04-24 19:19:53 +0200

    LoginServer: Adjust V4 migration

--- LoginServer/src/main/resources/db/migration/V4__Create_world_table.sql
+++ LoginServer/src/main/resources/db/migration/V4__Create_world_table.sql
@@ -1,5 +1,18 @@
 CREATE TABLE world(
-   id 		   int unsigned     auto_increment   PRIMARY KEY,
-   name        varchar(50)      NOT NULL,
-   url         varchar(255)     NOT NULL
+   id 		        int unsigned     auto_increment   PRIMARY KEY,
+   name             varchar(50)      NOT NULL,
+   url              varchar(255)     NOT NULL,
+   worldstatus_id   TINYINT unsigned NOT NULL DEFAULT 1
 );
+
+CREATE TABLE worldstatus(
+   id 		        TINYINT unsigned auto_increment   PRIMARY KEY,
+   description      varchar(50)      NOT NULL
+);
+
+ALTER TABLE world
+ADD CONSTRAINT worldstatus_id_fk FOREIGN KEY (worldstatus_id) REFERENCES login.worldstatus (id);
+
+insert into worldstatus (description) VALUES ('Online');
+insert into worldstatus (description) VALUES ('Offline');
+insert into worldstatus (description) VALUES ('Maintenance');
