c4690a1ef4a7275d4c6b4636142bc3e35d0ff845
Rickie Karp <rickie.karp@gmail.com>
2019-04-25 20:22:43 +0200

    Website: Add HttpFirewall bean. Fixes RequestRejectedException

--- Foundation/src/main/kotlin/net/rickiekarp/foundation/security/SecurityConfig.java
+++ Foundation/src/main/kotlin/net/rickiekarp/foundation/security/SecurityConfig.java
@@ -1,18 +1,25 @@
 package net.rickiekarp.foundation.security;
 
+import net.rickiekarp.foundation.config.BaseConfig;
 import net.rickiekarp.foundation.config.redis.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String[] NOT_SECURED = {"/account/authorize", "/account/create"};
+    private static final String[] NOT_SECURED = {
+            "/account/authorize",
+            "/account/create"
+    };
 
     @Autowired
     private CustomAuthenticationProvider authProvider;
@@ -24,14 +31,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers(NOT_SECURED).permitAll()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic()
-                .and()
-                .csrf().disable();
+        if (BaseConfig.Companion.get().getApplicationIdentifier().equals("Website")) {
+            http.csrf().disable().authorizeRequests().anyRequest().permitAll();
+        } else {
+            http
+                    .authorizeRequests()
+                    .antMatchers(NOT_SECURED).permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .csrf().disable();
+        }
+
     }
 
+    @Bean
+    public HttpFirewall defaultHttpFirewall() {
+        return new DefaultHttpFirewall();
+    }
 }
