eadacdd9797cf530d3aa8f895a5e3c16ed22e32a
Rickie Karp <rickie.karp@gmail.com>
2019-05-04 00:06:35 +0200

    Set spotbugs plugin version

--- base.gradle.kts
+++ base.gradle.kts
@@ -5,7 +5,7 @@ extra.set("kotlinVersion", "1.3.31")
 extra.set("dokkaVersion", "0.9.17")
 extra.set("detektVersion", "1.0.0-RC14")
 
-extra.set("spotbugsVersion", "3.1.12")
+extra.set("spotbugsPluginVersion", "1.7.1")
 
 extra.set("junitVersion", "5.4.2")
 
