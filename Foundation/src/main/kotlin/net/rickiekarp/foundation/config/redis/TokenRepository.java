74e19bfc32a50ed94713bb0a7584d9a338b3f64d
Rickie Karp <rickie.karp@gmail.com>
2019-05-06 20:51:46 +0200

    Migrate TokenRepository and OutputType to kotlin

--- Foundation/src/main/kotlin/net/rickiekarp/foundation/config/redis/TokenRepository.java
+++
@@ -1,8 +0,0 @@
-package net.rickiekarp.foundation.config.redis;
-
-import net.rickiekarp.foundation.model.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TokenRepository extends CrudRepository<User, String> {}
