3a4431f06d99a8450ca6d55fab2c2bfbe28945f5
Rickie Karp <rickie.karp@gmail.com>
2019-05-27 20:16:21 +0200

    HomeServer: Fix timestamp default value in V4 migration

--- HomeServer/src/main/resources/db/migration/V4__Create_application_settings_table.sql
+++ HomeServer/src/main/resources/db/migration/V4__Create_application_settings_table.sql
@@ -2,14 +2,14 @@
 
 ALTER TABLE shopping_note
 MODIFY COLUMN dateAdded TIMESTAMP,
-MODIFY COLUMN dateBought TIMESTAMP,
-MODIFY COLUMN lastUpdated TIMESTAMP;
+MODIFY COLUMN dateBought TIMESTAMP NULL default NULL,
+MODIFY COLUMN lastUpdated TIMESTAMP NULL default NULL;
 
--- Create AdminIp table
+-- Create applicationsettings table
 CREATE TABLE applicationsettings(
    id 		        int unsigned    auto_increment   PRIMARY KEY,
    identifier       varchar(50)     NOT NULL,
    content          TEXT            NOT NULL,
-   lastUpdated      TIMESTAMP
+   lastUpdated      TIMESTAMP       NULL default NULL
 );
 CREATE  INDEX i_identifier ON applicationsettings(identifier);
