3aebf713d8daefafd114ec0dbab8143bc84b7c44
Rickie Karp <rickie.karp@gmail.com>
2019-10-03 19:01:19 +0200

    homebackend: Remove Website dir

--- projects/homebackend/Website/bin/build-frontend.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-echo "Removing old website files..."
-rm -rf ../module-deployment/html
-
-echo "Building website files..."
-cd WebsiteFrontend
-ng build --prod --output-path=../../module-deployment/html
