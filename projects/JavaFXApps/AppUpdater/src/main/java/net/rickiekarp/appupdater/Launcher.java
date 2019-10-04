7ddf3944ae57c878fc0218df2a264f8908253baa
Rickie Karp <rickie.karp@gmail.com>
2019-10-05 00:00:45 +0200

    JavaFXApps: Fix compilation for all application

---
+++ projects/JavaFXApps/AppUpdater/src/main/java/net/rickiekarp/appupdater/Launcher.java
@@ -0,0 +1,8 @@
+package net.rickiekarp.appupdater;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        UpdateMain.main(args);
+    }
+}
---
+++ projects/JavaFXApps/BotManager/src/main/java/net/rickiekarp/botter/Launcher.java
@@ -0,0 +1,8 @@
+package net.rickiekarp.botter;
+
+public class Launcher {
+
+    public static void main(String[] args) {
+        MainApp.main(args);
+    }
+}
