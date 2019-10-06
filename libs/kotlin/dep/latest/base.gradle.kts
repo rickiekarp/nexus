b02d42d3a26d9e4fe534ef9fa828786e3a12d03d
Rickie Karp <rickie.karp@gmail.com>
2019-10-06 19:54:36 +0200

    Update base dependencies

--- libs/kotlin/dep/latest/base.gradle.kts
+++ libs/kotlin/dep/latest/base.gradle.kts
@@ -2,19 +2,19 @@
 extra.set("kotlinVersion", "1.3.50")
 
 // build system
-extra.set("gradleVersion", "5.5.1")
+extra.set("gradleVersion", "5.6.2")
 
 // testing
 extra.set("junitVersion", "5.5.2")
 
 // analysis
-extra.set("dokkaVersion", "0.9.17")
-extra.set("detektVersion", "1.0.0")
-extra.set("spotbugsPluginVersion", "2.0.0")
+extra.set("dokkaVersion", "0.9.18")
+extra.set("detektVersion", "1.0.1")
+extra.set("spotbugsPluginVersion", "3.1.12")
 
 // protocols
 extra.set("protobufGradlePlugin", "0.8.10")
-extra.set("protobufJava", "3.9.1")
+extra.set("protobufJava", "3.10.0")
 extra.set("protobufJavaFormat", "1.4")
 
 
