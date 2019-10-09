6cf712e21bc6b7780c2c156b3a7c69b52a097eed
Rickie Karp <rickie.karp@gmail.com>
2019-10-09 20:47:30 +0200

    homebackend: Remove unused code

--- projects/homebackend/HomeServer/src/main/kotlin/net/rickiekarp/homeserver/rest/api/ApplicationApi.kt
+++ projects/homebackend/HomeServer/src/main/kotlin/net/rickiekarp/homeserver/rest/api/ApplicationApi.kt
@@ -5,12 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import org.w3c.dom.Document
-import java.io.StringWriter
-import javax.xml.transform.OutputKeys
-import javax.xml.transform.TransformerFactory
-import javax.xml.transform.dom.DOMSource
-import javax.xml.transform.stream.StreamResult
 
 @RestController
 @RequestMapping("app")
@@ -34,23 +28,5 @@ class ApplicationApi {
     ): ResponseEntity<String> {
 //        val xmlString = FileParser.readFileAndReturnString(ServerContext.serverContext.getHomeDirPath(AppServer::class.java) + "/Resources/" + appIdentifier + "/changelog.xml")
         return ResponseEntity("not_implemented_yet", HttpStatus.OK)
-    }
-
-    private fun getDocumentString(doc: Document): String {
-        try {
-            val sw = StringWriter()
-            val tf = TransformerFactory.newInstance()
-            val transformer = tf.newTransformer()
-            transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no")
-            transformer.setOutputProperty(OutputKeys.METHOD, "xml")
-            transformer.setOutputProperty(OutputKeys.INDENT, "yes")
-            transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8")
-
-            transformer.transform(DOMSource(doc), StreamResult(sw))
-            return sw.toString()
-        } catch (ex: Exception) {
-            throw RuntimeException("Error converting to String", ex)
-        }
-
     }
 }
