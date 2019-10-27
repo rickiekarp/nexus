dba1d7add94bb7f9ca0774b92772b89660af8f7c
Rickie Karp <rickie.karp@gmail.com>
2019-10-27 17:07:26 +0100

    JavaFXApps: Migrate KeyboardHandler to kotlin

--- projects/JavaFXApps/SnakeFX/src/main/kotlin/net/rickiekarp/snakefx/util/KeyboardHandler.kt
+++ projects/JavaFXApps/SnakeFX/src/main/kotlin/net/rickiekarp/snakefx/util/KeyboardHandler.kt
@@ -1,41 +1,23 @@
-package net.rickiekarp.snakefx.util;
+package net.rickiekarp.snakefx.util
 
-import net.rickiekarp.snakefx.core.Direction;
-import net.rickiekarp.snakefx.viewmodel.ViewModel;
-import javafx.event.EventHandler;
-import javafx.scene.input.KeyEvent;
+import net.rickiekarp.snakefx.core.Direction
+import net.rickiekarp.snakefx.viewmodel.ViewModel
+import javafx.event.EventHandler
+import javafx.scene.input.KeyCode
+import javafx.scene.input.KeyEvent
 
 /**
  * This class handles the input of the user.
  */
-public class KeyboardHandler implements EventHandler<KeyEvent> {
-	private final ViewModel viewModel;
+class KeyboardHandler(private val viewModel: ViewModel) : EventHandler<KeyEvent> {
 
-	public KeyboardHandler(final ViewModel viewModel) {
-		this.viewModel = viewModel;
-	}
-
-	// also add WASD keys to keyboard handler due to https://bugs.openjdk.java.net/browse/JDK-8215545
-	@SuppressWarnings("incomplete-switch")
-	@Override
-	public void handle(final KeyEvent event) {
-		switch (event.getCode()) {
-			case W:
-			case UP:
-				viewModel.snakeDirection.set(Direction.UP);
-				break;
-			case A:
-			case LEFT:
-				viewModel.snakeDirection.set(Direction.LEFT);
-				break;
-			case S:
-			case DOWN:
-				viewModel.snakeDirection.set(Direction.DOWN);
-				break;
-			case D:
-			case RIGHT:
-				viewModel.snakeDirection.set(Direction.RIGHT);
-				break;
-		}
-	}
+    // also add WASD keys to keyboard handler due to https://bugs.openjdk.java.net/browse/JDK-8215545
+    override fun handle(event: KeyEvent) {
+        when (event.code) {
+            KeyCode.W, KeyCode.UP -> viewModel.snakeDirection.set(Direction.UP)
+            KeyCode.A, KeyCode.LEFT -> viewModel.snakeDirection.set(Direction.LEFT)
+            KeyCode.S, KeyCode.DOWN -> viewModel.snakeDirection.set(Direction.DOWN)
+            KeyCode.D, KeyCode.RIGHT -> viewModel.snakeDirection.set(Direction.RIGHT)
+        }
+    }
 }
