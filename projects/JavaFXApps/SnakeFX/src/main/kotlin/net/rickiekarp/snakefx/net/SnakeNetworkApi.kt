213797522a5b892740fe660c7454f3f8afa28291
Rickie Karp <rickie.karp@gmail.com>
2019-11-02 13:19:19 +0100

    Add gitlab-ci for go projects

--- projects/JavaFXApps/SnakeFX/src/main/kotlin/net/rickiekarp/snakefx/net/SnakeNetworkApi.kt
+++ projects/JavaFXApps/SnakeFX/src/main/kotlin/net/rickiekarp/snakefx/net/SnakeNetworkApi.kt
@@ -1,2 +1,17 @@
 package net.rickiekarp.snakefx.net
 
+import net.rickiekarp.core.net.NetworkAction
+import net.rickiekarp.core.net.NetworkApi
+import net.rickiekarp.core.net.provider.NetworkParameterProvider
+
+class SnakeNetworkApi : NetworkApi() {
+    companion object {
+        private val GAME_API_CONTEXT = "gamedata"
+        private val RANKING_DOMAIN = "ranking"
+        private val RANKING_GET_ACTION = "getRanking"
+
+        fun requestRanking(): NetworkAction {
+            return NetworkAction.Builder.create().setHost(GAME_API_CONTEXT).setDomain(RANKING_DOMAIN).setAction(RANKING_GET_ACTION).setMethod("GET").build()
+        }
+    }
+}
