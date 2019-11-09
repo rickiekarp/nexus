a178c88c6652a574c019368acf8512e4956cf21e
Rickie Karp <rickie.karp@gmail.com>
2019-11-09 20:33:09 +0100

    Add back ProgressBarComponent. Fixes build

--- projects/WebsiteFrontend/src/app/shared/shared.module.ts
+++ projects/WebsiteFrontend/src/app/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { CardComponent } from './components/card/card.component';
 import { TodolistComponent } from './components/todolist/todolist.component';
 import { TabsetComponent } from './components/tabset/tabset.component';
 import { TabContentComponent } from './components/tabset/tab-content/tab-content.component';
+import { ProgressBarComponent } from './components/progress-bar/progress-bar.component';
 import { FileTreeComponent } from './components/file-tree/file-tree.component';
 import { SwitchComponent } from './components/switch/switch.component';
 import { PellEditorComponent } from './components/pell-editor/pell-editor.component';
@@ -30,6 +31,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     TodolistComponent,
     TabsetComponent,
     TabContentComponent,
+    ProgressBarComponent,
     SwitchComponent,
     PellEditorComponent,
     AlertComponent,
@@ -42,6 +44,7 @@ import { ProfileComponent } from './components/profile/profile.component';
     TodolistComponent,
     TabsetComponent,
     TabContentComponent,
+    ProgressBarComponent,
     SwitchComponent,
     PellEditorComponent,
     AlertComponent,
