3de25f56cdace80388462cfc78881acca1bd363f
Rickie Karp <rickie.karp@gmail.com>
2020-03-22 20:13:51 +0100

    Update kotlin dependencies

--- libs/kotlin/dep/latest/base.gradle.kts
+++ libs/kotlin/dep/latest/base.gradle.kts
@@ -1,16 +1,16 @@
 // language
-extra.set("kotlinVersion", "1.3.61")
+extra.set("kotlinVersion", "1.3.70")
 
 // build system
 extra.set("gradleVersion", "6.1")
 
 // testing
-extra.set("junitVersion", "5.5.2")
+extra.set("junitVersion", "5.6.0")
 extra.set("mockitoCore", "3.2.4")
 
 // analysis
-extra.set("dokkaVersion", "0.10.0")
-extra.set("detektVersion", "1.3.1")
+extra.set("dokkaVersion", "0.10.1")
+extra.set("detektVersion", "1.6.0")
 extra.set("spotbugsPluginVersion", "3.1.12")
 
 // protocols
--- libs/kotlin/dep/latest/logging.gradle.kts
+++ libs/kotlin/dep/latest/logging.gradle.kts
@@ -1,2 +1,2 @@
-extra.set("log4jVersion", "2.13.0")
+extra.set("log4jVersion", "2.13.1")
 
