26da98dbf6da1f2b85370738ca0b99fce96ae928
Rickie Karp <rickie.karp@gmail.com>
2020-03-28 20:36:07 +0100

    Website: Replace Router with HashRouter

--- projects/Website/src/App.js
+++ projects/Website/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Router } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { HashRouter } from 'react-router-dom';
+import { createBrowserHistory, createHashHistory } from 'history';
 import { Chart } from 'react-chartjs-2';
 import { ThemeProvider } from '@material-ui/styles';
 import validate from 'validate.js';
@@ -12,7 +12,7 @@ import './assets/scss/index.scss';
 import validators from './common/validators';
 import Routes from './Routes';
 
-const browserHistory = createBrowserHistory();
+const hashHistory = createHashHistory();
 
 Chart.helpers.extend(Chart.elements.Rectangle.prototype, {
   draw: chartjs.draw
@@ -27,9 +27,9 @@ export default class App extends Component {
   render() {
     return (
       <ThemeProvider theme={theme}>
-        <Router history={browserHistory}>
+        <HashRouter history={hashHistory}>
           <Routes />
-        </Router>
+        </HashRouter>
       </ThemeProvider>
     );
   }
