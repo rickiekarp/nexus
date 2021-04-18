a3295ffb53b17b71e20a15c666395096ba53792d
Rickie Karp <contact@rickiekarp.net>
2021-04-18 18:48:13 +0200

    Update kotlin project dependencies

--- libs/kotlin/dep/latest/base.gradle.kts
+++ libs/kotlin/dep/latest/base.gradle.kts
@@ -1,21 +1,21 @@
 // language
-extra.set("kotlinVersion", "1.4.21")
+extra.set("kotlinVersion", "1.4.32")
 
 // build system
 extra.set("gradleVersion", "6.7.1")
 
 // testing
-extra.set("junitVersion", "5.7.0")
-extra.set("mockitoCore", "3.6.28")
+extra.set("junitVersion", "5.7.1")
+extra.set("mockitoCore", "3.9.0")
 
 // analysis
-extra.set("dokkaVersion", "1.4.10.2")
-extra.set("detektVersion", "1.15.0")
+extra.set("dokkaVersion", "1.4.30")
+extra.set("detektVersion", "1.16.0")
 extra.set("spotbugsPluginVersion", "4.6.0")
 
 // protocols
-extra.set("protobufGradlePlugin", "0.8.14")
-extra.set("protobufJava", "3.14.0")
+extra.set("protobufGradlePlugin", "0.8.15")
+extra.set("protobufJava", "3.15.8")
 extra.set("protobufJavaFormat", "1.4")
 
 
--- libs/kotlin/dep/latest/ui.gradle.kts
+++ libs/kotlin/dep/latest/ui.gradle.kts
@@ -1,2 +1,2 @@
 extra.set("javafxPlugin", "0.0.9")
-extra.set("javafxVersion", "15.0.1")
+extra.set("javafxVersion", "16")
