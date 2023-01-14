6a63bebdae47b601f81af4ab7b1f06a6d7f6f84c
Rickie Karp <contact@rickiekarp.net>
2023-01-14 23:54:22 +0100

    tree2yaml: Implement yaml loading functionality

--- tools/tree2yaml/main.go
+++ tools/tree2yaml/main.go
@@ -4,33 +4,73 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
+	"git.rickiekarp.net/rickie/home/tools/tree2yaml/extractor"
 	"git.rickiekarp.net/rickie/home/tools/tree2yaml/generator"
+	"git.rickiekarp.net/rickie/home/tools/tree2yaml/loader"
 	"gopkg.in/yaml.v2"
 )
 
-var rootDir string
-var flagCalcMd5 *bool
+var filePath string
 
 func main() {
 
-	flagCalcMd5 = flag.Bool("calcMd5", false, "calculate md5 sum of each file")
+	var flagCalcMd5 = flag.Bool("calcMd5", false, "calculate md5 sum of each file")
+	var flagGenerate = flag.Bool("generate", true, "generates a filelist")
+	var flagLoadList = flag.Bool("load", false, "loads an existing filelist")
+	var flagFindFiles = flag.String("findFilesIn", "", "finds files by a given search path, e.g. tree2yaml --load --findFilesIn=foo/bar /foo/bar")
+	var flagFindFolders = flag.String("findFoldersIn", "", "finds folders by a given search path, e.g. tree2yaml --load --findFoldersIn=foo/bar /foo/bar")
+
 	flag.Parse()
 
 	if flag.Args()[0] == "" {
 		os.Exit(1)
 	}
+	filePath = flag.Args()[0]
 
-	rootDir = flag.Args()[0]
-
-	tree := generator.BuildTree(rootDir, flagCalcMd5)
-
-	data, err := yaml.Marshal(&tree)
-
-	if err != nil {
-		fmt.Printf("Error while Marshaling. %v", err)
-		os.Exit(1)
+	// if flagLoadList was set to true, make sure we skip filelist generation
+	if *flagLoadList && *flagGenerate {
+		*flagGenerate = false
 	}
 
-	fmt.Println(string(data))
+	if *flagGenerate {
+		tree := generator.BuildTree(filePath, flagCalcMd5)
+
+		data, err := yaml.Marshal(&tree)
+
+		if err != nil {
+			fmt.Printf("Error while Marshaling. %v", err)
+			os.Exit(1)
+		}
+
+		fmt.Println(string(data))
+		os.Exit(0)
+	}
+
+	if *flagLoadList {
+		filelist := loader.LoadFilelist(filePath)
+
+		if len(*flagFindFiles) > 0 {
+			s := strings.Split(*flagFindFiles, "/")
+			folder := extractor.FindFolder(filelist.Tree.Folders, s)
+			if folder == nil {
+				os.Exit(0)
+			}
+			for _, file := range folder.Files {
+				fmt.Println(file.Name)
+			}
+		}
+
+		if len(*flagFindFolders) > 0 {
+			s := strings.Split(*flagFindFolders, "/")
+			folder := extractor.FindFolder(filelist.Tree.Folders, s)
+			if folder == nil {
+				os.Exit(0)
+			}
+			for _, folder := range folder.Folders {
+				fmt.Println(folder.Name)
+			}
+		}
+	}
 }
