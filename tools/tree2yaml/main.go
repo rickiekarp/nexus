bc6436d1f3debbc024358f2e9af4fcae5d32d782
Rickie Karp <contact@rickiekarp.net>
2023-01-14 13:13:05 +0100

    tree2yaml: Add build target for amd64

--- tools/tree2yaml/main.go
+++ tools/tree2yaml/main.go
@@ -3,19 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"path"
-	"path/filepath"
-	"sort"
 
-	"git.rickiekarp.net/rickie/home/tools/tree2yaml/hash"
-	"git.rickiekarp.net/rickie/home/tools/tree2yaml/model"
-	"git.rickiekarp.net/rickie/home/tools/tree2yaml/sorting"
+	"git.rickiekarp.net/rickie/home/tools/tree2yaml/generator"
 	"gopkg.in/yaml.v2"
 )
-
-var Version = "development" // Version set during go build using ldflags
 
 var rootDir string
 var flagCalcMd5 *bool
@@ -31,7 +23,7 @@ func main() {
 
 	rootDir = flag.Args()[0]
 
-	tree := BuildTree(rootDir)
+	tree := generator.BuildTree(rootDir, flagCalcMd5)
 
 	data, err := yaml.Marshal(&tree)
 
@@ -41,67 +33,4 @@ func main() {
 	}
 
 	fmt.Println(string(data))
-}
-
-func BuildTree(dir string) *model.FileTree {
-	dir = path.Clean(dir)
-
-	var filetree *model.FileTree = &model.FileTree{}
-	var tree *model.Folder
-	var nodes = map[string]interface{}{}
-	var walkFunc filepath.WalkFunc = func(p string, info os.FileInfo, err error) error {
-		if info.IsDir() {
-			nodes[p] = &model.Folder{
-				Name:    path.Base(p),
-				Files:   []*model.File{},
-				Folders: []*model.Folder{},
-			}
-		} else {
-			var md5 = ""
-			if *flagCalcMd5 {
-				md5 = hash.CalcMd5(p)
-			}
-
-			filetree.Size += info.Size()
-
-			nodes[p] = &model.File{
-				Name:         path.Base(p),
-				Size:         info.Size(),
-				LastModified: info.ModTime(),
-				Md5:          md5,
-			}
-		}
-
-		return nil
-	}
-
-	err := filepath.Walk(dir, walkFunc)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for key, value := range nodes {
-		var parentFolder *model.Folder
-		if key == dir {
-			tree = value.(*model.Folder)
-			continue
-		} else {
-			parentFolder = nodes[path.Dir(key)].(*model.Folder)
-		}
-
-		switch v := value.(type) {
-		case *model.File:
-			parentFolder.Files = append(parentFolder.Files, v)
-			sort.Sort(sorting.ByName(parentFolder.Files))
-		case *model.Folder:
-			parentFolder.Folders = append(parentFolder.Folders, v)
-			sort.Sort(sorting.ByFolderName(parentFolder.Folders))
-		}
-	}
-
-	filetree.RootDir = rootDir
-	filetree.ParserVersion = Version
-	filetree.Tree = tree
-
-	return filetree
 }
