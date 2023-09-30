e412e68c815829947d5fdb2624bd388535f31574
Rickie Karp <rickie.karp@yandex.com>
2023-09-30 19:39:56 +0200

    homebackend: Removed old API's

--- projects/homebackend/LoginServer/src/main/kotlin/net/rickiekarp/loginserver/dto/RecoveryCredentials.kt
+++
@@ -1,7 +0,0 @@
-package net.rickiekarp.loginserver.dto
-
-open class RecoveryCredentials {
-    var username: String? = null
-    var lostPasswordHash: String? = null
-    var newPassword: String? = null
-}
