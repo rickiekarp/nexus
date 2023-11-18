1dfcf731cc5ba13b1b4cb7c37f4fe48c6dabd303
Rickie Karp <rickie.karp@yandex.com>
2023-11-18 19:18:47 +0100

    Add webpage placeholder endpoints

--- internal/nucleus/webpage/contact.go
+++ internal/nucleus/webpage/contact.go
@@ -5,8 +5,21 @@ import (
 	"net/http"
 )
 
+type Contact struct {
+	Name     string `json:"name"`
+	Email    string `json:"email"`
+	Role     string `json:"role"`
+	Location string `json:"location"`
+}
+
 func ServeContactInfo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	json.NewEncoder(w).Encode("ServeContactInfo")
+	contactInfo := Contact{
+		Name:     "Rickie Karp",
+		Email:    "contact@rickiekarp.net",
+		Role:     "Software Developer",
+		Location: "Karlsruhe",
+	}
+	json.NewEncoder(w).Encode(contactInfo)
 }
