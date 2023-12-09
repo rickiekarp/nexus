9bf37487f15a34d5f08b6aac03a6e84cf1686f75
Rickie Karp <rickie.karp@yandex.com>
2023-12-09 16:35:24 +0100

    Add logrotate config

--- tools/scripts/project6/update-checker.sh
+++ tools/scripts/project6/update-checker.sh
@@ -1,22 +1,12 @@
 #! /bin/bash
 
-logToFileEnabled=true
-logFile="/var/log/pi/project6svc.log"
 lockFile="/tmp/project6svc.lock"
-binaryRoot="/home/pi/operation/project6"
-
-function log() {
-	if [[ $logToFileEnabled = true ]]; then
-		echo "$(date) $1" >> $logFile
-	else 
-		echo "$(date) $1"
-	fi
-}
+binaryRoot="/home/rickie/Programs/Dev/project6"
+project6LogFile="/var/log/project6.log"
 
 if [[ ! -f $lockFile ]]; then
-	log "Could not find process lock file! Starting service"
-    cd $binaryRoot
-	./project6svc &
+   	cd $binaryRoot
+	./project6svc -log=$project6LogFile -host=rickiekarp.net:12000 &
 	exit 0
 fi
 
@@ -27,17 +17,14 @@ isProcessRunning=$(ps aux | awk '{print $2 }' | grep -w $pid)
 
 # if the process it not running, switch to the new binary and start the service again
 if [[ ! -n $isProcessRunning ]]; then
-	log "Service is not currently running!"
-
     cd $binaryRoot
 
 	# if an update file exists, update to the new build
 	if [[ -f project6svc_update ]]; then
-		log "Updating to new deployment service!"
 		rm project6svc
 		mv project6svc_update project6svc
+		chmod +x project6svc
 	fi
 
-	log "Starting project6 service"
-	./project6svc &
+	./project6svc -log=$project6LogFile -host=rickiekarp.net:12000 &
 fi
